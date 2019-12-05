20f76a0d4c3efa52f02513f116b4334076ca6a23
Alberto Ivo <albertoivo@gmail.com>
2019-12-05 17:45:45 -0300

    setores brasil alpha vantage

--- stocks.py
+++ stocks.py
@@ -5,29 +5,29 @@ financeiros = ['BBDC4.SA', 'BBDC3.SA', 'ITUB4.SA', 'ITUB3.SA', 'BBAS3.SA',
                'BBSE3.SA', 'PSSA3.SA', 'MULT3.SA', 'IGTA3.SA', 'BRML3.SA']
 
 bens_industriais = ['WEGE3.SA', 'EMBR3.SA', 'AZUL4.SA',
-                    'CCRO3.SA', 'ECOR3.SA', 'GOLL4.SA', 'RAIL3.SA']
+                    'CCRO3.SA', 'ECOR3.SA', 'GOLL4.SA', 'RAIL3.SA',]
 
-consumo_nao_basico = ['BRFS3.SA', 'JBSS3.SA', 'MRFG3.SA']
+consumo_nao_basico = ['BRFS3.SA', 'JBSS3.SA', 'MRFG3.SA',]
 
-consumo_nao_ciclico = ['ABEV3.SA', 'PCAR4.SA', 'NATU3.SA']
+consumo_nao_ciclico = ['ABEV3.SA', 'PCAR4.SA', 'NATU3.SA',]
 
 consumo_ciclico = ['BTOW3.SA', 'LAME4.SA', 'LREN3.SA',
-                   'MGLU3.SA', 'VVAR3.SA', 'CYRE3.SA', 'MRVE3.SA', 'CVCB3.SA']
+                   'MGLU3.SA', 'VVAR3.SA', 'CYRE3.SA', 'MRVE3.SA', 'CVCB3.SA',]
 
-diversos = ['ESTC3.SA', 'KROT3.SA', 'RENT3.SA', 'SMLS3.SA']
+diversos = ['ESTC3.SA', 'KROT3.SA', 'RENT3.SA', 'SMLS3.SA',]
 
 materiais_basicos = ['KLBN11.SA', 'SUZB3.SA', 'BRAP4.SA', 'VALE3.SA',
                      'BRKM5.SA', 'GGBR4.SA', 'GOAU4.SA', 'CSNA3.SA',
-                     'USIM3.SA', 'USIM5.SA']
+                     'USIM3.SA', 'USIM5.SA',]
 
-petroleo = ['CSAN3.SA', 'PETR3.SA', 'PETR4.SA', 'BRDT3.SA', 'UGPA3.SA']
+petroleo = ['CSAN3.SA', 'PETR3.SA', 'PETR4.SA', 'BRDT3.SA', 'UGPA3.SA',]
 
-saude = ['HYPE3.SA', 'RADL3.SA', 'FLRY3.SA', 'QUAL3.SA']
+saude = ['HYPE3.SA', 'RADL3.SA', 'FLRY3.SA', 'QUAL3.SA',]
 
-telecomunicacao = ['TIMP3.SA', 'VIVT4.SA']
+telecomunicacao = ['TIMP3.SA', 'VIVT4.SA',]
 
 utilidade_publica = ['SBSP3.SA', 'CMIG4.SA', 'ELET3.SA', 'ELET6.SA',
-                     'ENBR3.SA', 'EGIE3.SA', 'EQTL3.SA', 'TAEE11.SA']
+                     'ENBR3.SA', 'EGIE3.SA', 'EQTL3.SA', 'TAEE11.SA',]
 
 ibov = financeiros + bens_industriais + consumo_nao_basico + \
     consumo_ciclico + consumo_nao_ciclico + diversos + materiais_basicos + \
